350c56b48bae8e926d41a2113fc03f8f0cb42cd6
Jekabs Krigerts <jekabs.krigerts@gmail.com>
2022-11-28 22:06:22 +0200

    Semantic HTML and login link in homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,16 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{ mix('css/home/home.css') }}">
-    <title>Home</title>
+    <title>Home - Autool</title>
 </head>
 <body>
     {{-- <a class="link" href="{{ url('/login') }}">Login</a> --}}
-    <div class="main">
-        <div class="main__nav">
-            <div class="main__nav__box">
-                <a href="{{url('/')}}" class="main__nav__box--logo">Autool</a>
+    <header class="header">
+        <div class="header__nav">
+            <div class="header__nav__box">
+                <a href="{{url('/')}}" class="header__nav__box--logo">Autool</a>
+                @guest
+                    <a class="header__nav__box--login" href="{{ url('/login') }}">Login</a>
+                @endguest
             </div>
         </div>
-    </div>
+    </header>
+    <main></main>
 </body>
 </html>
