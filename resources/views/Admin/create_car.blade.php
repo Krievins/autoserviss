93eaf52ed6d768cb11b3be1b981ff6441918bd32
Toms Ricars Krievins <toms@wtf.lv>
2022-12-01 09:24:39 +0200

    Created Cars Functions and Database Migrations

---
+++ resources/views/Admin/create_car.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.main', [
+    'stylesheet' => mix('css/home/home.css'),
+    'title' => 'Admin - Create Car'
+])
+
+@section('content')
+    <p>Admin</p>
+    <a href="{{ url('admin/create_user') }}">Create User</a>
+    <a href="{{ url('admin/users') }}">Users</a>
+    <h1 style="margin-top: 40px;">Create Car</h1>
+    <form action="/admin/create_car" method="post">
+        @csrf
+        {{-- Owner Name --}}
+        <label for="owner_name">Īpašnieka Vārds</label>
+        <input type="text" name="owner_name" id="owner_name" required>
+        {{-- Owner Phone Number --}}
+        <label for="owner_phone">Īpašnieka Telefona Nummurs</label>
+        <input type="text" name="owner_phone" id="owner_phone" required>
+        {{-- Number Plate --}}
+        <label for="number_plate">Mašīnas Numura Zīme</label>
+        <input type="text" name="number_plate" id="number_plate" required>
+        {{-- Release Year --}}
+        <label for="release_year">Izlaiduma Gads</label>
+        <input type="text" name="release_year" id="release_year" required>
+        {{-- Description --}}
+        <label for="descript">Darāmais</label>
+        <input type="text" name="descript" id="descript" required>
+        {{-- Button --}}
+        <button type="submit">Submit</button>
+    </form>
+@endsection
